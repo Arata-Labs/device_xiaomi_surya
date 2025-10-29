4830c9573c7f7d0a2d34deccdf3b6a32aca73c50
anky894 <ko282ko82@gmail.com>
2025-10-29 12:26:27 +0530

    sweet: Import perf boost configs from vendor
    Change-Id: I53a2f94833924a04fd34101919e781b4c8dead0b
    Signed-off-by: therealmharc <therealmharc@gmail.com>
    Signed-off-by: HinohArata <mmgcntk@gmail.com>

--- device.mk
+++ device.mk
@@ -81,6 +81,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/perf/perfboostsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfboostsconfig.xml \
+    $(LOCAL_PATH)/configs/perf/perfconfigstore.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfconfigstore.xml \
     $(LOCAL_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # Sensors
