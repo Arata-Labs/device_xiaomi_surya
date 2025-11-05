6cba34e0e8fd7dd6e8d28460ebd77057919f7b53
therealmharc <therealmharc@gmail.com>
2025-11-05 16:43:23 +0000

    sweet: Add Torch Strength support [1/2]
    Signed-off-by: therealmharc <therealmharc@gmail.com>
    Signed-off-by: HinohArata <mmgcntk@gmail.com>

--- lineage_surya.mk
+++ lineage_surya.mk
@@ -15,6 +15,9 @@ $(call inherit-product, device/xiaomi/surya/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Additional flags
+TORCH_STR_SUPPORTED := true
+
 PRODUCT_NAME := lineage_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
