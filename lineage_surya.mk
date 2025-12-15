81b016a78270bbdf3da99932db2bbcbdb989e339
HinohArata <mmgcntk@gmail.com>
2025-12-15 09:16:04 +0700

    Initial OrionOS bringup
    Signed-off-by: HinohArata <mmgcntk@gmail.com>

--- lineage_surya.mk
+++ lineage_surya.mk
@@ -15,6 +15,13 @@ $(call inherit-product, device/xiaomi/surya/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# OrionOS flags
+ORION_MAINTAINER := Skyy丨アラタ
+ORION_GAPPS := true
+WITH_GMS := true
+TARGET_ENABLE_BLUR := true
+TARGET_BOOT_ANIMATION_RES := 1080
+
 # Additional flags
 TORCH_STR_SUPPORTED := true
 TARGET_BUILD_DEVICE_AS_WEBCAM := true
